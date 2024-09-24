17cd35b87f1f483cdf239dedbdaf48ef684bbb99
Ruchika De Zoysa <ruchika.dezoysa@gmail.com>
2024-09-24 11:26:22 +1000

    Commit 1

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 
+# create a function to do the below for each dataset in the folder
 
 
-with open("C:\Innovation project\HousingProject\Melbourne_housing_FULL.csv", "r") as f:
+with open("Melbourne_housing_FULL.csv", "r") as f:
     data = pd.read_csv(f)
 
 # Print the first few rows to verify the data
@@ -12,3 +13,8 @@ print(data.head())
 # Display basic information about the dataset
 print(data.describe())
 print(data.info())
+
+# Select the following columns and save it to another Excel if not exists 
+# ['Suburb', 'Address', 'Rooms', 'Type', 'Price', 'Method', 'SellerG', 'Date', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car', 'Landsize', 'BuildingArea', 'YearBuilt', 'CouncilArea', 'Lattitude', 'Longtitude', 'Regionname', 'Propertycount']
+data = data[['Suburb', 'Address', 'Rooms', 'Type', 'Price', 'Method', 'SellerG', 'Date', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car', 'Landsize', 'BuildingArea', 'YearBuilt', 'CouncilArea', 'Lattitude', 'Longtitude', 'Regionname', 'Propertycount']]
+data.to_csv("Melbourne_housing_FULL.csv", index=False)
